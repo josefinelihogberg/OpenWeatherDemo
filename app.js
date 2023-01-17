9691863c3117888602ced9c2f2049870c78d43eb
josefine <leexue417@gmail.com>
2023-01-17 18:57:12 +0100

    Version2

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-$(".search").on("click", askCoordValue);
+$(".weather-table thead").append(`
+<tr>
+<th>Location</th>
+<th>Description</th>
+<th>Temperature</th>
+<th>Feels like temp</th>
+<th>Cloud</th>
+<th>Humidity</th>
+<th>Air pressure</th>
+<th>Wind speed</th>
+</tr>
+`
+);
 
-function renderApp(city, des, temp, feels, humidity, wind) {
-    let tableBody = $(".table-body");
-    tableBody.append(`
+function renderWeather (data) {
+    $(".weather-table tbody").append(`
     <tr>
-    <td>${city}</th>
-    <td>${des}</td>
-    <td>${temp}</td>
-    <td>${feels}</td>
-    <td>${humidity}</td>
-    <td>${wind}</td>
+     <td>${data.location}</td>
+     <td>${data.description}</td>
+     <td>${data.temp}°C</td>
+     <td>${data.feelTemp}°C</td>
+     <td>${data.cloud}</td>
+     <td>${data.humidity}</td>
+     <td>${data.pressure}</td>
+     <td>${data.wind}</td>
     </tr>
     `)
 }
+
+$(".location-field").on("keydown", event=> {
+    if(event.key == "Enter") {
+        cityName = $(".location-field").val();
+        console.log(cityName);
+        searchWeather(cityName)//调用searchWeather函数，传入用户输入的城市名称。 因为是async异步函数，所以返回一个Promise对象。
+        .then(renderWeather)//能用then()方法说明searchWeather函数返回一个Promise对象，即weatherData  ！拿到weatherdata以后，调用回调函数renderWeather，以处理weatherData。
+        .catch(err => $("body").text("Something went wrong!")); //如果拿不到weatherdata，返回了错误信息时的处理方法
+}})
--- weatherService.js
+++ weatherService.js
@@ -1,22 +1,42 @@
-let apiKey = "282fa25ff7e477b0ff526c36cdcc0786";
-function askCoordValue(){
-    let city =$(".city").val();
-    let askCoordUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
-    $.get(askCoordUrl, data => {
-         let lat = data[0].lat.toFixed(2);
-         let lon = data[0].lon.toFixed(2);
-  askWeather(lat,lon);
-    });
+let APIKey = "282fa25ff7e477b0ff526c36cdcc0786";
+
+function fetchJson(url) {
+ return fetch(url).then(resp=>resp.json());//返回的是Promise对象
 }
-function askWeather(lat, lon) {
-    let askWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-    $.get(askWeatherUrl, data => {
-       let city = data.name;
-       let description = data.weather[0].description;
-       let temp = data.main.temp + "°C";
-       let feels_like = data.main.feels_like;
-       let humidity = data.main.humidity + "%";
-       let wind = data.wind.speed;
-       renderApp(city, description,temp,feels_like,humidity,wind);
-    })
+//创建findCityCoord函数，输入城市名，返回jSon格式的城市坐标
+function findCityCoord(city) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+    return fetchJson(url);
+}
+//创建extractCoord函数，把fetch Promise对象里的值带出来处理
+function extractCoords(data) {
+    return {
+      lat: data.coord.lat,
+      lon: data.coord.lon
+    }
+  }
+function findWeather(coords) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&units=metric&appid=${APIKey}`;
+    return fetchJson(url);
+}
+function transformWeatherData(data) {
+    return {
+        location:data.name,
+        description: data.weather[0].description,
+        temp: data.main.temp.toFixed(0),
+        feelTemp:data.main.feels_like.toFixed(0),
+        cloud: data.clouds.all,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        wind:data.wind.speed
+    }
+}
+
+//创建异步函数seachWeather，返回一个Promise对象，即weatherData
+async function searchWeather(city) { //异步函数searchWeather用来返回Promise结果weatherData
+let coords = await findCityCoord(city).then(extractCoords); // await返回异步函数findCityCoord的返回值，即一个Promise对象。then()方法用来处理该Promise对象的返回值--能用的jSON格式的response,将坐标提取出来
+console.log(coords);
+let weatherData = await findWeather(coords).then(transformWeatherData); // await返回一个Promise,即所有天气信息数据。调用函数transformWeatherData以提取出需要的天气属性
+console.log(weatherData);
+    return weatherData;
 }
